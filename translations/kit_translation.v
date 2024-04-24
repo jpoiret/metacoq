8bb1775d3cb8ac5adf64beebc18d711864e2fc7a
Josselin Poiret <dev@jpoiret.xyz>
2024-04-24 14:32:19 +0200

    Turn param_kit into the "usual" parametricity translation.

--- translations/kit_translation.v
+++ translations/kit_translation.v
@@ -60,40 +60,49 @@ Definition relAnon := {| binder_name := nAnon ; binder_relevance := Relevant |}.
 Definition relNamed (n : ident) := {| binder_name := nNamed n ; binder_relevance := Relevant |}.
 
 
+Fixpoint double_context_up_to (n : nat) (t : term) : term :=
+  match t with
+  | tRel k => if Nat.leb n k then tRel (1 + (2 * (k - n)) + n) else t
+  | tProd a A B => tProd a (double_context_up_to n A) (double_context_up_to (1+n) B)
+  | tApp f l => tApp (double_context_up_to n f) (List.map (double_context_up_to n) l)
+  | tLambda b A t => tLambda b (double_context_up_to n A) (double_context_up_to (1+n) t)
+  | _ => t
+  end.
+
 Definition param_kit : TransKit
  :=
  {| ctx := list term;
     ty := term × term;
     tm := term × term;
     emp_ctx := [];
-    ext_ctx := (fun Γ '(T , Tparam) => Γ ,, T ,, tApp Tparam [tRel 0]);
+    ext_ctx := (fun Γ '(T , Tparam) => Γ ,, (double_context_up_to 0 T) ,, tApp (lift0 1 Tparam) [tRel 0]);
     pop_ctx Γ := match Γ with
       | [] => []
       | [a] => []
       | _ :: _ :: Γ => Γ
       end;
-    var_tm Γ := (tRel 1, tRel 0);
-    wk_tm Γ '(a , b) := (lift0 2 a,lift0 2 b);
+    var_tm Γ := (tRel 0, tRel 0);
+    wk_tm Γ '(a , b) := (lift0 1 a,lift0 2 b);
     el Γ '(a , b) := (a , b);
     sort_tsl Γ s := let u := tSort s in (u , tLambda (relNamed "A") u (tProd relAnon (tRel 0) (tSort s)));
 
     prod_tsl Γ a '(A , Aparam) '(B , Bparam) :=
-    let u := tProd a A (tProd (paramBinder a) (tApp (lift0 1 Aparam) [tRel 0]) B) in
-    (u , tLambda (relNamed "f") u (tProd a (lift0 1 A) (tProd (paramBinder a) (tApp (lift0 2 Aparam) [tRel 0]) (tApp (lift 1 2 Bparam) [tApp (tRel 2) [tRel 1; tRel 0]]))));
+    let A' := double_context_up_to 0 A in
+    let B' := double_context_up_to 1 B in
+    (tProd a A B , tLambda (relNamed "f") (tProd a A' B') (tProd a (lift0 1 A') (tProd (paramBinder a) (tApp (lift0 2 Aparam) [tRel 0]) (tApp (lift 1 2 Bparam) [tApp (tRel 2) [tRel 1]]))));
 
     app_tsl Γ '(f , fparam) '(a , aparam) :=
-    let u := tApp f [a ; aparam] in
-    (u , tApp fparam [a ; aparam]);
+    (tApp f [a] , tApp fparam [(double_context_up_to 0 a) ; aparam]);
 
     lambda_tsl Γ b '(A , Aparam) '(t , tparam) :=
-    let u := tLambda b A (tLambda (paramBinder b) (tApp (lift0 1 Aparam) [tRel 0]) t) in
-    (u , tLambda b A (tLambda (paramBinder b) (tApp (lift0 1 Aparam) [tRel 0]) tparam))|}.
+    let u := tLambda b A t in
+    let A' := double_context_up_to 0 A in
+    (u , tLambda b A' (tLambda (paramBinder b) (tApp (lift0 1 Aparam) [tRel 0]) tparam))|}.
 
-Definition test := forall (A : Type), A -> A -> A.
+Definition test := fun (A B : Type) (f : A -> B) (a : A) => f a.
 
 MetaCoq Run (tm <- (tmEval all test >>= tmQuote) ;;
              let '(u , v) := (tsl_kit_tm (k := param_kit) [] tm) in
-             tmPrint u ;;
-             (tmEval all v >>= tmPrint) ;;
+             (* tmEval all v >>= tmPrint ;; *)
              unq <- tmUnquote v ;;
              tmEval all unq.(my_projT2) >>= tmPrint).
